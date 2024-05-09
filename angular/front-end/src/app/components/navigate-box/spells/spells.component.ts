5cfecc077e443dc324b05b821b13e1cec6e2f2a4
Karol Wójciak <karix.wojciak@gmail.com>
2024-05-09 22:07:55 +0200

    Add basic versions of the spells and skills components

--- angular/front-end/src/app/components/navigate-box/spells/spells.component.ts
+++ angular/front-end/src/app/components/navigate-box/spells/spells.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CharacterDataService } from '../../../services/character-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-spells',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spells.component.html',
   styleUrl: './spells.component.css'
 })
 export class SpellsComponent {
+
+  spells: any[] = [];
+
+  constructor(private characterDataService: CharacterDataService) { }
+
+  ngOnInit(): void {
+    this.characterDataService.getCharacters().subscribe({
+          next: (data) => {
+            this.spells = data.characters[0].data.spells;
+            console.log(this.spells);
+          },
+          error: (err) => {
+            console.error('Failed to fetch spells data', err);
+          }
+    });
+  }
 
 }
--- angular/front-end/src/app/components/skills/skills.component.ts
+++ angular/front-end/src/app/components/skills/skills.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CharacterDataService } from '../../services/character-data.service';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  skills: object = {};
 
+  constructor(private characterDataService: CharacterDataService) { }
+
+  ngOnInit(): void {
+    this.characterDataService.getCharacters().subscribe({
+          next: (data) => {
+            this.skills = data.characters[0].data.Skills;
+            console.log(this.skills);
+          },
+          error: (err) => {
+            console.error('Failed to fetch skills data', err);
+          }
+    });
+  }
 }
