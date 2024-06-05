b4b8526b379c3000e8013bcd4cbf804e80d6a416
Przemyslaw Maresz <przemyslaw.maresz@gmail.com>
2024-06-05 17:01:36 +0200

    moved main page from app.component to character-board.component

--- angular/front-end/src/app/app.routes.ts
+++ angular/front-end/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NavigateBoxComponent } from './components/navigate-box/navigate-box.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { CharacterBoardComponent } from './components/character-board/character-board.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'character-board', component: CharacterBoardComponent},
     {path: 'browse', component: NavigateBoxComponent},
     {path: '', redirectTo:'', pathMatch:'full'},
     {path: '**', redirectTo:''},
