fd3e15de96c6e1ac44a336e56df42e693952d05a
Mateusz Mirecki (mamireck) <mamireck@cisco.com>
2024-06-05 18:41:33 +0200

    style: savingThrows, base and sigle stats

--- angular/front-end/src/app/components/base-stats/base-stats.component.ts
+++ angular/front-end/src/app/components/base-stats/base-stats.component.ts
@@ -12,14 +12,20 @@ export class BaseStatsComponent {
   @Input() value: any;  // Define an input property to receive data
   @Input() description: any
 
+  total: number = 0;
+
     // Method to increment the value
     incrementValue() {
       this.value += 1;
+      this.total += 1;
+
     }
   
     // Method to decrement the value
     decrementValue() {
       this.value -= 1;
+      this.total -= 1;
+
     }
     
 }
