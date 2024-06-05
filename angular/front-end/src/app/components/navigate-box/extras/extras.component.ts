a8a1204190ab87773bbe69b77e559ee0b004ad9c
Michał Koczkodaj <92547392+mkoczkodaj@users.noreply.github.com>
2024-06-05 21:07:49 +0200

    Update extras.component.ts

--- angular/front-end/src/app/components/navigate-box/extras/extras.component.ts
+++ angular/front-end/src/app/components/navigate-box/extras/extras.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './extras.component.css'
 })
 export class ExtrasComponent {
-
+  skill1 = { name: 'Fireball', description: 'Casts a ball of fire dealing damage to enemies.' };
+  skill2 = { name: 'Teleport', description: 'Instantly move to a nearby location.' };
+  skill3 = { name: 'Shield', description: 'Creates a magical shield that absorbs damage.' };
 }
