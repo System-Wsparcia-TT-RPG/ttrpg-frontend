f5907c096542efb8add9d04b22f42d02db4b46d7
Przemyslaw Maresz <przemyslaw.maresz@gmail.com>
2024-06-05 18:29:23 +0200

    added login and register forms

--- angular/front-end/src/app/components/login/login.component.ts
+++ angular/front-end/src/app/components/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  async login() {
+    if (this.loginForm.valid) {
+      const userData = this.loginForm.value;
+      const userName = userData.username;
+      const password = userData.password;
+      // TODO: Implement login logic
+    }
+  }
 }
--- angular/front-end/src/app/components/register/register.component.ts
+++ angular/front-end/src/app/components/register/register.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  async register() {
+    if (this.registerForm.valid) {
+      const userData = this.registerForm.value;
+      const uusername = userData.username;
+      const email = userData.email;
+      const password = userData.password;
+      // TODO: Implement register logic
+    }
+  }
 }
