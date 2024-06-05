7c70f9985166a441b8dc7d67b7cf52a846051916
Mateusz Mirecki (mamireck) <mamireck@cisco.com>
2024-06-05 19:56:08 +0200

    conditions

--- angular/front-end/src/app/components/conditions-list/conditions-list.component.ts
+++ angular/front-end/src/app/components/conditions-list/conditions-list.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CharacterDataService } from '../../services/character-data.service';
 
 @Component({
   selector: 'app-conditions-list',
@@ -8,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './conditions-list.component.css'
 })
 export class ConditionsListComponent {
+  hit_dice: string = "No current hit dice";
+  damage_dice: any = {}
+  constructor(private characterDataService: CharacterDataService) { }
+
+  ngOnInit(): void {
+    this.characterDataService.getCharacter(1).subscribe({
+          next: (data) => {
+            this.hit_dice = data[0].combat["hit_dice"];
+            this.damage_dice = data[0].race.actions[0]["damage_dice"];
+            // console.log(this.hit_dice)
+            console.log(this.damage_dice)
+
+          },
+          error: (err) => {
+            console.error('Failed to fetch skills data', err);
+          }
+    });
+  }
 
 }
