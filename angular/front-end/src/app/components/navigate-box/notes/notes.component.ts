ee9964edada6e20db45f8bc0581f720ea1d9db00
Przemyslaw Maresz <przemyslaw.maresz@gmail.com>
2024-06-06 18:23:44 +0200

    hot fix for notes.component

--- angular/front-end/src/app/components/navigate-box/notes/notes.component.ts
+++ angular/front-end/src/app/components/navigate-box/notes/notes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.css'
 })
