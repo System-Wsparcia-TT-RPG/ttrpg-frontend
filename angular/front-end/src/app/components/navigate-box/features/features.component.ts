b47dec62c6b8c3085ab8db7b59000a167b553f94
Michał Koczkodaj <92547392+mkoczkodaj@users.noreply.github.com>
2024-06-06 16:16:05 +0200

    Update features.component.ts

--- angular/front-end/src/app/components/navigate-box/features/features.component.ts
+++ angular/front-end/src/app/components/navigate-box/features/features.component.ts
@@ -8,12 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './features.component.css'
 })
 export class FeaturesComponent {
-  feature1: string = 'Strength';
-  feature1Description: string = 'Strength is a measure of your character\'s physical power. It affects how much they can lift, how hard they can hit, and their general physical prowess. High strength can be crucial for warriors and characters who engage in a lot of physical combat.';
-
-  feature2: string = 'Dexterity';
-  feature2Description: string = 'Dexterity represents agility, reflexes, and balance. It influences a character\'s ability to dodge attacks, move silently, and perform tasks that require finesse. Rogues and archers typically rely on high dexterity for their skills and attacks.';
-
-  feature3: string = 'Constitution';
-  feature3Description: string = 'Constitution is a measure of your character\'s overall health and stamina. It affects their hit points and resistance to physical ailments. A high constitution is important for all characters, especially those who find themselves frequently in the midst of battle.';
+  features = [
+    {
+      name: 'Strength',
+      description: 'Strength is a measure of your character\'s physical power. It affects how much they can lift, how hard they can hit, and their general physical prowess. High strength can be crucial for warriors and characters who engage in a lot of physical combat.'
+    },
+    {
+      name: 'Dexterity',
+      description: 'Dexterity represents agility, reflexes, and balance. It influences a character\'s ability to dodge attacks, move silently, and perform tasks that require finesse. Rogues and archers typically rely on high dexterity for their skills and attacks.'
+    },
+    {
+      name: 'Constitution',
+      description: 'Constitution is a measure of your character\'s overall health and stamina. It affects their hit points and resistance to physical ailments. A high constitution is important for all characters, especially those who find themselves frequently in the midst of battle.'
+    }
+  ];
 }
