77eafa1e508aa9b7fb0810de55c74ae56ddf0c93
LukZawadzki <lzawadzki@student.agh.edu.pl>
2024-06-06 14:18:59 +0200

    Saving throws with editing

--- angular/front-end/src/app/components/saving-throws/saving-throws.component.ts
+++ angular/front-end/src/app/components/saving-throws/saving-throws.component.ts
@@ -1,45 +1,26 @@
 import { Component } from '@angular/core';
 import { CharacterDataService } from '../../services/character-data.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-saving-throws',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './saving-throws.component.html',
   styleUrl: './saving-throws.component.css'
 })
 export class SavingThrowsComponent {
-  savingThrows: savingThrows = {
-    strength: 0,
-    dexterity: 0,
-    constitution: 0,
-    intelligence: 0,
-    wisdom: 0,
-    charisma: 0,
-  };
-
-
+  character: any = {}
   constructor(private characterDataService: CharacterDataService) { }
 
   ngOnInit(): void {
-    this.characterDataService.getCharacter(1).subscribe({
-          next: (data) => {
-            this.savingThrows = data[0].saving_throws as savingThrows;
-          },
-          error: (err) => {
-            console.error('Failed to fetch skills data', err);
-          }
-    });
+    this.character = this.characterDataService.currentCharacter;
   }
 
-}
-
-interface savingThrows {
-  strength: number;
-  dexterity: number;
-  constitution: number;
-  intelligence: number;
-  wisdom: number;
-  charisma: number;
+  updateSavingThrow(savingThrow: string, value: number): void {
+    if (this.character) {
+      this.character.saving_throws[savingThrow] = value;
+    }
+  }
 }
