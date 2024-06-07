43f9ad5cde7e9eafe9ed87542a59c297e81dfc44
Mateusz Mirecki (mamireck) <mamireck@cisco.com>
2024-06-07 10:09:40 +0200

    Senses component

--- angular/front-end/src/app/components/senses/senses.component.ts
+++ angular/front-end/src/app/components/senses/senses.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common'; 
 import { CharacterDataService } from '../../services/character-data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-senses',
   standalone: true,
-  imports: [HttpClientModule, CommonModule ], 
+  imports: [FormsModule, CommonModule ], 
   templateUrl: './senses.component.html',
   styleUrls: ['./senses.component.css']
 })
@@ -19,6 +19,40 @@ export class SensesComponent implements OnInit {
 
   ngOnInit() {
     this.character = this.characterDataService.currentCharacter;
-    // console.log('Current character in senses:', this.character);
+    console.log('Current character in senses:', this.character?.race.senses);
+  }
+
+    updateSenses(savingThrow: string, value: number): void {
+    if (this.character) {
+      this.character.saving_throws[savingThrow] = value;
+    }
   }
 }
+
+
+// import { Component } from '@angular/core';
+// import { CharacterDataService } from '../../services/character-data.service';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-saving-throws',
+//   standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './saving-throws.component.html',
+//   styleUrl: './saving-throws.component.css'
+// })
+// export class SavingThrowsComponent {
+//   character: any = {}
+//   constructor(private characterDataService: CharacterDataService) { }
+
+//   ngOnInit(): void {
+//     this.character = this.characterDataService.currentCharacter;
+//   }
+
+//   updateSavingThrow(savingThrow: string, value: number): void {
+//     if (this.character) {
+//       this.character.saving_throws[savingThrow] = value;
+//     }
+//   }
+// }
